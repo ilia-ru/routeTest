0bc255f00a7bf0efe01fa1ffd434129035b5d1e8
111 Name <111@example.com>
2020-08-15 22:21:51 +0300

    Делаем шаг 2 без удаления лишних

--- src/routes.java
+++ src/routes.java
@@ -11,6 +11,7 @@ public class routes
 
     public void run(point[] inPoint)
     {
+        // Step 1. Формируем массив маршрутов, добавляем - LT & RT
         for(i=1; i<inPoint.length; i++)  // по LT
         {
             currentY = inPoint[i].getY();
@@ -24,6 +25,44 @@ public class routes
 
             }
         }
+        if(tmpRoutes.stream().count() <=0)  // нет горизонталей
+        {
+            System.out.println("Невозможно построить маршруты - нет горизонталей");
+            return;
+        }
+
+        // Step. 2. Ищем LB. Перебираем массив маршрутов построчно.
+        // Для каждой LT подбираем LB, чтобы LT.x == LB.x и LB.y < LT.y
+        route tmp;
+        i=0;
+        int numOftmpRoutes = tmpRoutes.size();
+        while(i < numOftmpRoutes)  // по маршрутам, созданным выше (кроме добавляемых здесь)
+        {
+//            currentY = inPoint[i].getY();
+//            currentX = inPoint[i].getX();
+            tmp = null;
+            for (j = 1; j < inPoint.length; j++)  // по входным данным
+            {
+                if ((tmpRoutes.get(i).getLt().getX() == inPoint[j].getX())
+                        && (inPoint[j].getY() < tmpRoutes.get(i).getLt().getY()))
+                {
+                    if(tmp == null)
+                    {// дополняем LB в текущую строку
+                        tmp = tmpRoutes.get(i);
+                        tmp.setLb(inPoint[j]);
+                    }
+                    else {   // добавляем новую строку - новый вариант маршрута и в нее LB
+                        tmp = tmpRoutes.get(i);
+                        tmpRoutes.add(new route(tmp.getLt(), tmp.getRt(), inPoint[j], tmp.getRb()));
+                   }
+                }
+            }
+            i++;
+            // Удаляем строки для которых не нашлось LB
+           // tmpRoutes.remove(i);
+
+        }
+        // Выводим результат
         i=1;
         for(route r : tmpRoutes)
         {
